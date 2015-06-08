068aa58ae511609a30b439954f4400e5b191bdb5
firstfu <firefirstfu@gmail.com>
2015-06-08 20:43:42 +0800

    Initial Commit

---
+++ 單字卡/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  單字卡
+//
+//  Created by OSX on 2015/6/8.
+//  Copyright (c) 2015年 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 單字卡/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  單字卡
+//
+//  Created by OSX on 2015/6/8.
+//  Copyright (c) 2015年 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
